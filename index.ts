9a40f210d7162649191d9fde17fa7cf43a095223
sugeul <sugeul.jin@gmail.com>
2023-11-14 16:27:07 +0900

    add follow func

--- index.ts
+++ index.ts
@@ -1,5 +1,32 @@
 import {PI, _n, arccos, radian} from './util';
 
+const generate_sub_basis = (basis:number) => {
+    return Array(Math.floor(basis)).fill(null).map((_,i) => basis/(i+1));
+}
+
+const generate_shorter_common_multiple = (base = 7, max=144) => {
+    let ret = [];
+    for (let i = base; i < max; i += base) {
+        ret.push(i);
+    }
+
+    return ret;
+}
+
+const generate_2_4_multiple = (max:number) => {
+    return [max * 2, max * 4];
+}
+
+const unique = (val:any, idx:number, arr:any[]) => arr.indexOf(val) === idx;
+
+const regulate = (val: number, min: number, max: number) => {
+    if (val < min){
+        return min - (min - val) / (max - min) * min;
+    }else {
+        return val;
+    }
+}
+
 export class OutOfAmplitudeRangeException extends Error {
     constructor () {
         super();
@@ -18,9 +45,9 @@ export class Basis {
     private phase_init: radian;
     /** @description the length of one vibrate cycle in radian scale. ( \lambda ) wavelength = 1 / (angular velocity) */
     private wavelength: radian;
-    
+
     constructor ( 
-        phase_init = 0 as radian, amplitude:number = 1, wavelength = PI * 2 as radian,
+        phase_init = 0 as radian, amplitude:number = 1, wavelength = PI * 2 as radian
     ) {
         this.phase_init = phase_init;
         this.amplitude = amplitude;
@@ -30,7 +57,7 @@ export class Basis {
     public set =  ({
         phase_init, amplitude, wavelength
     }: {
-        phase_init?:radian, amplitude?:number, wavelength?: radian
+        phase_init?:radian, amplitude?:number, wavelength?: radian, data?: [radian, number][]
     }) => {
         if (phase_init !== undefined) { this.phase_init = phase_init; };
         if (amplitude  !== undefined) { this.amplitude = amplitude };
@@ -87,7 +114,18 @@ export class Basis {
         return errors.reduce( (lhs, rhs) => lhs + rhs, 0 ) / errors.length;
     }
 
-    public fit = (samples: [radian, number][], max_slit = 4, max_amplitude=1.0, step=10) => {
+    public fit = (options: {
+        samples: [radian, number][], max_slit:number, max_amplitude:number, step:number
+    }|[radian, number][], ...others: any) => {
+        if (Array.isArray(options) ) {
+            return this._fit(options, ...others);
+        }else {
+            return this._fit(options.samples, options.max_slit, options.max_amplitude, options.step);
+        }
+    }
+
+    private _fit = (samples: [radian, number][], max_slit = 4, max_amplitude=1.0, step=10) => {
+        
         let [min_mse_value, min_mse_amptd, min_mse_phase] = [-1, 0, 0 as radian];
     
         const [phase_global_max, phase_global_min] = [2 * PI as radian, 0 as radian];
@@ -97,13 +135,13 @@ export class Basis {
         let   [amptd_local_max,  amptd_local_min ] = [amptd_global_max, amptd_global_min];
 
         for (let i = 0; i< step; i ++) {
-            for (let phase_init = phase_local_min; phase_init < phase_local_max * (max_slit+1) / max_slit;
-                phase_init = phase_init + (phase_local_max - phase_local_min) / max_slit as radian) {
+            for (let j = 0; j < max_slit; j += 1) {
+                let phase_init = phase_local_min + (phase_local_max - phase_local_min) / max_slit * j as radian;
                 
                 this.set({phase_init})
     
-                for (let amplitude = amptd_local_min; amplitude < amptd_local_max * (max_slit+1) / max_slit;
-                    amplitude = amplitude + (amptd_local_max - amptd_local_min) / max_slit) {
+                for (let k = 0; k < max_slit; k += 1) {
+                    let amplitude = amptd_local_min + (amptd_local_max - amptd_local_min) / max_slit * k;
                     
                     this.set({amplitude})
     
@@ -133,9 +171,13 @@ export class Base {
     private base: Basis[];
     private offset: number;
 
+    /** @description stored data to fit incrementally */
+    private data: [radian, number][];
+
     constructor (base: [radian, number, radian][], offset = 0) {
         this.base = base.map(([phase_init, amplitude, wavelength]) => new Basis(phase_init, amplitude, wavelength));
         this.offset = offset;
+        this.data = [];
     }
 
     public getAll = (theta: radian) => {
@@ -160,12 +202,22 @@ export class Base {
         return errors.reduce( (lhs, rhs) => lhs + rhs, 0 ) / errors.length;
     }
 
-    public fit = (given_samples: [radian, number][], max_slit=4, step = 10, dropout = 0.0005) => {
+    public fit = (options: {
+        given_samples: [radian, number][], max_slit:number, step:number, dropout: number
+    }|[radian, number][], ...others: any) => {
+        if (Array.isArray(options) ) {
+            return this._fit(options, ...others);
+        }else {
+            return this._fit(options.given_samples, options.max_slit, options.step, options.dropout);
+        }
+    }
+
+    public _fit = (given_samples: [radian, number][], max_slit=4, step = 10, dropout = 0.0005) => {
         this.offset = given_samples.map(([x,y]) => y).reduce((a,b)=> a+b,0) / given_samples.length;
 
         let samples = given_samples.map(([x, y]) => ([x, y - this.offset] as [radian, number]));
 
-        this.base.forEach( (basis) => {
+        this.base.sort((BaseA, BaseB) => BaseB.asObject().wavelength - BaseA.asObject().wavelength).forEach( (basis) => {
             let max_amplitude = Math.max( ...samples.map(([x,y]) => Math.abs(y)) )
 
             basis.fit(samples, max_slit, max_amplitude, step);
@@ -174,7 +226,46 @@ export class Base {
 
         this.base = this.base.filter(basis => basis.asObject().amplitude > dropout)
 
+        this.data = [...given_samples];
+
         return this;
+    }
+
+    public follow = (
+        {
+            wavelengths: given_wavelengths=[], ignorable=[],
+            start_from = 14, gap = 7, tail = 14, further = 28, max_slit = 4, step=10, dropout= -1
+        }:{
+            wavelengths: number[], ignorable: number[],
+            start_from: number, gap: number, tail: number, further: number, max_slit: number, step: number, dropout: number
+        }) => {
+        let base = new Base([], 0);
+        let ret: number[] = Array(start_from+gap).fill(0);
+        
+        for (let i = start_from; i < this.data.length; i++ ){
+            const wavelengths = (given_wavelengths.length === 0
+                ?[
+                    ...generate_sub_basis(7),
+                    ...generate_shorter_common_multiple(7, Math.min(i+1, tail)),
+                    ...generate_2_4_multiple(Math.min(i+1, tail))
+                ].filter(unique)
+                :given_wavelengths
+            ).map(wavelength => [0, 0, wavelength as radian] as [radian, number, radian]);
+    
+            base = new Base(wavelengths, 0);
+            base.fit({given_samples: this.data.slice(Math.max(i+1 - tail,0),i+1).filter(([idx, val]) => !ignorable.includes(idx)), max_slit, step, dropout});
+
+            let max = Math.max(...this.data.slice(Math.max(i+1 - tail,0),i+1).map(([_, num]) => num));
+            let min = Math.min(...this.data.slice(Math.max(i+1 - tail,0),i+1).map(([_, num]) => num));
+            ret.push(regulate(base.get(i + gap as radian), min, max));
+        }
+
+        for (let i = this.data.length; i < this.data.length + further; i++ ) {
+            ret.push(base.get(i+gap as radian));
+        }
+
+
+        return ret;
     }
 
     public asObject = () => {
--- test.js
+++ test.js
@@ -1,15 +1,42 @@
 const fs = require('fs');
 const {Base} = require('./index');
 
+const generate_sub_basis = (basis) => {
+    return Array(Math.floor(basis)).fill(null).map((_,i) => basis/(i+1));
+}
+
+const generate_shorter_common_multiple = (base = 7, max=144) => {
+    let ret = [];
+    for (let i = base; i < max; i += base) {
+        ret.push(i);
+    }
+
+    return ret;
+}
+
+const generate_2_4_multiple = (max) => {
+    return [max * 4];
+}
+
+const unique = (val, idx, arr) => arr.indexOf(val) === idx;
+
 const dots = JSON.parse(fs.readFileSync('./sample/data.json', 'utf8'));
 
 const data = dots.map((dot, i) => [i, dot]);
 
 //let base = Base.from_wavelengthes([296, 112, 14, 7, 7/2, 2])
-let base = Base.from_wavelengthes([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,28,35,42,49,56,63,70,77,84,91,98,105,112,119,126,133,140,147,154,161,168,336])
+let base = Base.from_wavelengthes([
+    ...generate_sub_basis(7),
+    ...generate_shorter_common_multiple(7, data.length),
+    ...generate_2_4_multiple(data.length)
+].filter(unique))
 
-base.fit(data, 10, 2, 50000)
+base.fit(data, 4, 3, 4000)
 
-console.log(base.asObject().map(basis => basis.wavelength))
+let start_from = 3, gap = 7, further = 50, tail = 35, ignorable = [];
+let followed = base.follow({start_from, gap, further, tail, max_slit: 4, step: 2, dropout: 5000, ignorable})
 
-fs.writeFileSync('./sample/expectation.tsv', data.map(([x, y]) => [x, y, base.get(x)].join('\t')).join('\n'))
+fs.writeFileSync('./sample/expectation.tsv',
+    data.map(([x, y]) => [x, y, Math.floor(base.get(x)), Math.floor(followed[x])].join('\t')).concat(
+    new Array(further).fill(null).map((_,i) => [data[data.length - 1][0]+i+1, 0, 0, Math.floor(followed[data[data.length - 1][0]+i+1])].join('\t'))
+).join('\n'))
